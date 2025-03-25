7a0ac000c58415578ba1d9d97fe1f40198063cd5
jeanpierrebroz <jeanpierrebroz@gmail.com>
2025-03-25 11:57:46 -0600

    made angular project, initial commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]   // Add this line
 })
 export class AppComponent {
-  title = 'ragnetic';
+  title = "ragnetic";
 }
